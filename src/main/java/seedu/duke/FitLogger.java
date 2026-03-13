b99ccfcdc40ebb5dbb76145155a2a72979a188d9
Javierseow <seowjackiejavier@gmail.com>
2026-03-13 09:37:20 +0800

    Add template Parser class

--- src/main/java/seedu/duke/FitLogger.java
+++ src/main/java/seedu/duke/FitLogger.java
@@ -2,15 +2,21 @@ package seedu.duke;
 
 public class FitLogger {
     private Ui ui;
+    private Parser parser;
 
     public FitLogger() {
         ui = new Ui();
+        parser = new Parser();
     }
 
     public void run() {
         ui.showWelcome();
         String command = ui.readCommand();
+        String[] temps = parser.parse(command);
         ui.temporaryOutput(command);
+        for (String temp : temps) {
+            System.out.println(temp);
+        }
         ui.showGoodbye();
     }
 
