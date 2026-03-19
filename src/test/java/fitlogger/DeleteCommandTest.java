4ebe182ca052392375948997f363da2f89db5207
Javierseow <seowjackiejavier@gmail.com>
2026-03-19 23:25:03 +0800

    Fix package import for DeleteCommandTest

--- src/test/java/fitlogger/DeleteCommandTest.java
+++ src/test/java/fitlogger/DeleteCommandTest.java
@@ -11,6 +11,7 @@ import org.junit.jupiter.api.Test;
 import fitlogger.command.DeleteCommand;
 import fitlogger.workout.Running;
 import fitlogger.workoutlist.WorkoutList;
+import fitlogger.ui.Ui;
 
 class DeleteCommandTest {
     @Test
